e3a4050f2e04ab19a3eaacfe77826f511cc71b6e
Max Bladen-Clark <max.bladen-clark@defra.gov.uk>
2023-09-21 13:06:37 +0100

    add tests for <Availability/>

--- __tests__/client/lib/classnames.test.js
+++ __tests__/client/lib/classnames.test.js
@@ -1,4 +1,36 @@
+import { classnames } from '../../../src/client/lib/classnames'
+
 describe('classnames', () => {
-  // it removes falsey classnames
-  // it removes classnames which are not strings.
+  it('removes falsey class names from returned string', () => {
+    // Arrange
+    const classArray = [
+      'valid-class',
+      false,
+      null,
+      undefined,
+      0
+    ]
+
+    // Act
+    const className = classnames(...classArray)
+
+    // Assert
+    expect(className).toEqual('valid-class')
+  })
+
+  it('removes non-string class names from returned string', () => {
+    // Arrange
+    const classArray = [
+      'valid-class',
+      1,
+      [],
+      {}
+    ]
+
+    // Act
+    const className = classnames(...classArray)
+
+    // Assert
+    expect(className).toEqual('valid-class')
+  })
 })
