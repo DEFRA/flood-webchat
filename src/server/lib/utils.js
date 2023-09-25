e1309ae5b06188c393325999d9c8247b9d096efb
Ash Taylor <ashtaylor@ip-192-168-1-247.eu-west-2.compute.internal>
2023-09-25 09:34:35 +0100

    HOTFIX fix start/end time availability

--- src/server/lib/utils.js
+++ src/server/lib/utils.js
@@ -3,8 +3,8 @@ const isWithinHours = days => {
   const name = now.toLocaleDateString('en-GB', { weekday: 'long' })
   const day = days.find(d => d.day.toLowerCase() === name.toLowerCase())
   const date = now.toLocaleDateString('en-GB').split('/')
-  const open = `${date[2]}-${date[1]}-${date[0]}T${day.openTime}.000Z`
-  const close = `${date[2]}-${date[1]}-${date[0]}T${day.closeTime}.000Z`
+  const open = `${date[2]}-${date[1]}-${date[0]}T${day.openTime}`
+  const close = `${date[2]}-${date[1]}-${date[0]}T${day.closeTime}`
 
   return now.getTime() >= Date.parse(open) && now.getTime() <= Date.parse(close)
 }
