7da892db7b633a5e294d8dd046cd4c1ad4c7b2c4
Ash Taylor <ashtaylor@ip-192-168-1-247.eu-west-2.compute.internal>
2023-10-23 13:39:33 +0100

    FSR-990 sanitize messages

--- src/client/lib/transform-messages.js
+++ src/client/lib/transform-messages.js
@@ -1,7 +1,9 @@
+import sanitizeHtml from 'sanitize-html'
+
 export const transformMessage = (message) => {
   return {
     id: message.id,
-    text: message.messageContent?.text,
+    text: sanitizeHtml(message.messageContent?.text),
     createdAt: new Date(message.createdAt),
     user: message.authorEndUserIdentity?.fullName?.trim() || null,
     assignee: message.authorUser?.firstName || null,
