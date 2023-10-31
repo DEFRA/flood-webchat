2db9bd600c4e0442633a4d5d3c81e8459ea6b5bc
Ash Taylor <ashtaylor@ip-192-168-1-247.eu-west-2.compute.internal>
2023-10-31 08:38:24 +0000

    FSR-990 tidy logs

--- src/client/store/appReducer.jsx
+++ src/client/store/appReducer.jsx
@@ -17,34 +17,28 @@ export const THEAD_ID_STORAGE_KEY = 'webchat_thread_id'
 export const appReducer = (state, action) => {
   const { type, payload } = action
 
+  console.log(type, state, payload)
+
   switch (type) {
     case 'SET_CHAT_VISIBILITY':
-      console.log('SET_CHAT_VISIBILITY', state, payload)
-
       return {
         ...state,
         isChatOpen: payload
       }
 
     case 'SET_CHAT_REQUESTED':
-      console.log('SET_CHAT_REQUESTED', state)
-
       return {
         ...state,
         isChatRequested: true
       }
 
     case 'SET_AVAILABILITY':
-      console.log('SET_AVAILABILITY', state, payload)
-
       return {
         ...state,
         availability: payload
       }
 
     case 'SET_CUSTOMER_ID':
-      console.log('SET_CUSTOMER_ID', state, payload)
-
       if (payload) {
         window.localStorage.setItem(CUSTOMER_ID_STORAGE_KEY, payload)
       } else {
@@ -58,8 +52,6 @@ export const appReducer = (state, action) => {
       }
 
     case 'SET_THREAD_ID':
-      console.log('SET_THREAD_ID', state, payload)
-
       if (payload) {
         window.localStorage.setItem(THEAD_ID_STORAGE_KEY, payload)
       } else {
@@ -72,8 +64,6 @@ export const appReducer = (state, action) => {
       }
 
     case 'SET_MESSAGE': {
-      console.log('SET_MESSAGE', state, payload)
-
       return {
         ...state,
         messages: [...state.messages, payload]
@@ -81,32 +71,24 @@ export const appReducer = (state, action) => {
     }
 
     case 'SET_MESSAGES':
-      console.log('SET_MESSAGES', state, payload)
-
       return {
         ...state,
         messages: payload.reverse()
       }
 
     case 'SET_AGENT':
-      console.log('SET_AGENT', state, payload)
-
       return {
         ...state,
         agent: payload
       }
 
     case 'SET_AGENT_TYPING':
-      console.log('SET_AGENT_TYPING', state, payload)
-
       return {
         ...state,
         isAgentTyping: payload
       }
 
     case 'SET_AGENT_STATUS':
-      console.log('SET_AGENT_STATUS', state, payload)
-
       return {
         ...state,
         agentStatus: payload
