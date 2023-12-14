a84a0fbf28d85c3be8dad578b684d39070af7300
LeeGordon83 <lee.gordon83@gmail.com>
2023-12-14 11:05:17 +0000

    more changes

--- src/client/components/screens/feedback.jsx
+++ src/client/components/screens/feedback.jsx
@@ -7,61 +7,60 @@ export function Feedback (onCancel) {
       <PanelHeader />
 
       <div className='wc-body'>
-       <h3 id='wc-subtitle' className='govuk-heading-s'>Give Feedback</h3>
-       <div className="govuk-form-group">
-        <fieldset className='govuk-fieldset govuk-!-margin-bottom-4'>
-          <legend className='govuk-fieldset__legend'>
-          Overall, how did you feel about your webchat
-          </legend>
-          <div className="govuk-radios govuk-radios--small" data-module="govuk-radios">
-            <div className="govuk-radios__item">
-              <input
-                  id="satisfied"
-                  name="satisfaction"
-                  type="radio"
-                  className="govuk-radios__input"
-                  value="Satisfied"
+        <h3 id='wc-subtitle' className='govuk-heading-s'>Give Feedback</h3>
+        <div className='govuk-form-group'>
+          <fieldset className='govuk-fieldset govuk-!-margin-bottom-4'>
+            <legend className='govuk-fieldset__legend'>
+              Overall, how did you feel about your webchat
+            </legend>
+            <div className='govuk-radios govuk-radios--small' data-module='govuk-radios'>
+              <div className='govuk-radios__item'>
+                <input
+                  id='satisfied'
+                  name='satisfaction'
+                  type='radio'
+                  className='govuk-radios__input'
+                  value='Satisfied'
                 />
 
-              <label className="govuk-label govuk-radios__label" for="satisfied">Satisfied</label>
-            </div>
-            <div className="govuk-radios__item">
-                <input
-                    id="no-opinion"
-                    name="satisfaction"
-                    type="radio"
-                    className="govuk-radios__input"
-                    value="No opinion"
-                  />
-
-                <label className="govuk-label govuk-radios__label" for="no-opinion">No Opinion</label>
+                <label className='govuk-label govuk-radios__label' for='satisfied'>Satisfied</label>
               </div>
-            <div className="govuk-radios__item">
+              <div className='govuk-radios__item'>
                 <input
-                      id="dissatisfied"
-                      name="satisfaction"
-                      type="radio"
-                      className="govuk-radios__input"
-                      value="Dissatisfied"
-                  />
+                  id='no-opinion'
+                  name='satisfaction'
+                  type='radio'
+                  className='govuk-radios__input'
+                  value='No opinion'
+                />
 
-                <label className="govuk-label govuk-radios__label" for="dissatisfied">Dissatisfied</label>
+                <label className='govuk-label govuk-radios__label' for='no-opinion'>No Opinion</label>
+              </div>
+              <div className='govuk-radios__item'>
+                <input
+                  id='dissatisfied'
+                  name='satisfaction'
+                  type='radio'
+                  className='govuk-radios__input'
+                  value='Dissatisfied'
+                />
+
+                <label className='govuk-label govuk-radios__label' for='dissatisfied'>Dissatisfied</label>
+              </div>
             </div>
-          </div>
-        </fieldset>
-      </div>
-        <div className="govuk-form-group">
-          <label className="govuk-label" for="improvements">
+          </fieldset>
+        </div>
+        <div className='govuk-form-group'>
+          <label className='govuk-label' for='improvements'>
             How can we improve webchat? (optional)
           </label>
-          <textarea className="govuk-textarea govuk-!-margin-bottom-4" id="improvements" name="improvements" rows="5"></textarea>
+          <textarea className='govuk-textarea govuk-!-margin-bottom-4' id='improvements' name='improvements' rows='5' />
         </div>
         <div className='govuk-button-group'>
           <a id='feedback-save' href='#' className='govuk-button govuk-!-font-size-16' data-module='govuk-button'>Submit</a>
           <a id='feedback-cancel' href='#' className='govuk-link govuk-!-font-size-16' data-module='govuk-button'>Cancel</a>
         </div>
-    </div>
+      </div>
     </>
   )
 }
-
