387dc09080aea010d74ea813f855e78661ed3112
LeeGordon83 <lee.gordon83@gmail.com>
2025-01-08 15:59:02 +0000

    constants.js ref

--- src/client/store/app/AppProvider.jsx
+++ src/client/store/app/AppProvider.jsx
@@ -2,7 +2,7 @@ import React, { createContext, useEffect, useReducer, useMemo } from 'react'
 // import { ChatEvent } from '@nice-devone/nice-cxone-chat-web-sdk'
 
 import { initialState, reducer } from './reducer.js'
-import { CUSTOMER_ID_STORAGE_KEY, THREAD_ID_STORAGE_KEY, SETTINGS_STORAGE_KEY, MESSAGES_UNSEEN_COUNT } from './constants.js'
+import { CUSTOMER_ID_STORAGE_KEY, THREAD_ID_STORAGE_KEY, SETTINGS_STORAGE_KEY, MESSAGES_UNSEEN_COUNT } from '../constants.js'
 
 export const AppContext = createContext(initialState)
 
